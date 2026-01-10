349a9db11c7a4aa588d94c5dc3467dc5c020c916
Metadon-svg <krisevicu@gmail.com>
2026-01-10 19:36:03 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,4 @@
+// build.gradle.kts УРОВНЯ ПРИЛОЖЕНИЯ (app)
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -6,6 +7,9 @@ plugins {
 android {
     namespace = "com.metadon.music"
     compileSdk = 34
+
+    // Эта версия NDK предустановлена на серверах GitHub Actions
+    ndkVersion = "25.1.8937393"
 
     defaultConfig {
         applicationId = "com.metadon.music"
@@ -42,6 +46,7 @@ android {
     }
 
     composeOptions {
+        // Версия, которую ты просил
         kotlinCompilerExtensionVersion = "1.5.8"
     }
 
@@ -58,17 +63,18 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     
+    // Jetpack Compose
     val composeBom = platform("androidx.compose:compose-bom:2023.10.01")
     implementation(composeBom)
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.compose.ui:ui-tooling-preview")
     
-    // Media3 (Плеер)
+    // Media3 (Для музыки и стриминга)
     implementation("androidx.media3:media3-exoplayer:1.2.0")
     implementation("androidx.media3:media3-session:1.2.0")
     implementation("androidx.media3:media3-ui:1.2.0")
     
-    // Coil (Обложки)
+    // Coil (Загрузка обложек из интернета)
     implementation("io.coil-kt:coil-compose:2.5.0")
 }
