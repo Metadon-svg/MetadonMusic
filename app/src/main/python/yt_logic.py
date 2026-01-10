eb9d4b8551caef6443fcae789bfbb504bbefdc44
Metadon-svg <krisevicu@gmail.com>
2026-01-10 21:11:25 +0300

    Update yt_logic.py

--- app/src/main/python/yt_logic.py
+++ app/src/main/python/yt_logic.py
@@ -4,25 +4,22 @@ import yt_dlp
 yt = YTMusic()
 
 def search_music(query):
-    # Ищем песни в YouTube Music
-    results = yt.search(query, filter="songs")
-    output = []
-    for r in results:
-        output.append({
-            "id": r['videoId'],
-            "title": r['title'],
-            "artist": r['artists'][0]['name'],
-            "cover": r['thumbnails'][-1]['url']
-        })
-    return output
+    try:
+        results = yt.search(query, filter="songs")
+        output = []
+        for r in results:
+            output.append({
+                "id": str(r['videoId']),
+                "title": str(r['title']),
+                "artist": str(r['artists'][0]['name']),
+                "cover": str(r['thumbnails'][-1]['url'])
+            })
+        return output
+    except:
+        return []
 
 def get_stream_url(video_id):
-    # Получаем прямую ссылку на аудио-поток
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'quiet': True,
-        'no_warnings': True,
-    }
+    ydl_opts = {'format': 'bestaudio/best', 'quiet': True}
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(f"https://www.youtube.com/watch?v={video_id}", download=False)
         return info['url']
