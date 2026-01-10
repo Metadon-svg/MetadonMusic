07f394765fc337f307800e0512f1d7dd63b99d05
Metadon-svg <krisevicu@gmail.com>
2026-01-10 21:58:35 +0300

    Update yt_logic.py

--- app/src/main/python/yt_logic.py
+++ app/src/main/python/yt_logic.py
@@ -6,19 +6,19 @@ yt = YTMusic()
 def search_music(query):
     try:
         results = yt.search(query, filter="songs")
-        return [{"id": r['videoId'], "title": r['title'], "artist": r['artists'][0]['name'], 
-                 "artistId": r['artists'][0]['id'], "cover": r['thumbnails'][-1]['url']} for r in results]
+        return [{"id": str(r['videoId']), "title": str(r['title']), "artist": str(r['artists'][0]['name']), 
+                 "artistId": str(r['artists'][0]['id']), "cover": str(r['thumbnails'][-1]['url'])} for r in results]
     except: return []
 
 def get_artist_songs(artist_id):
     try:
         artist_data = yt.get_artist(artist_id)
         results = artist_data['songs']['results']
-        return [{"id": r['videoId'], "title": r['title'], "artist": artist_data['name'], 
-                 "cover": r['thumbnails'][-1]['url']} for r in results]
+        return [{"id": str(r['videoId']), "title": str(r['title']), "artist": str(artist_data['name']), 
+                 "cover": str(r['thumbnails'][-1]['url'])} for r in results]
     except: return []
 
 def get_stream_url(video_id):
     ydl_opts = {'format': 'bestaudio/best', 'quiet': True}
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        return ydl.extract_info(f"https://www.youtube.com/watch?v={video_id}", download=False)['url']
+        return str(ydl.extract_info(f"https://www.youtube.com/watch?v={video_id}", download=False)['url'])
