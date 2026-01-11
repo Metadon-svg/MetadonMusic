7f7225f9d34b1e6a8d73f842ff03598aedc92457
Metadon-svg <krisevicu@gmail.com>
2026-01-11 11:05:04 +0300

    Update MainActivity.kt

--- app/src/main/java/com/metadon/music/MainActivity.kt
+++ app/src/main/java/com/metadon/music/MainActivity.kt
@@ -4,21 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Инициализация Python только для веса (yt_logic.py должен лежать в папке python)
+        
+        // Python для веса
         if (!Python.isStarted()) {
             Python.start(AndroidPlatform(this))
         }
 
         setContent {
-            MaterialTheme(colorScheme = darkColorScheme()) {
+            MaterialTheme {
                 MainAppScreen()
             }
         }
