2d61e7de24ba28f4e3bacdcea17d11831e594585
Metadon-svg <krisevicu@gmail.com>
2026-01-11 11:28:40 +0300

    Update Models.kt

--- app/src/main/java/com/metadon/music/Models.kt
+++ app/src/main/java/com/metadon/music/Models.kt
@@ -5,11 +5,5 @@ data class Track(
     val title: String,
     val artist: String,
     val cover: String,
-    val duration: Int = 0
-)
-
-data class Playlist(
-    val id: Int,
-    val name: String,
-    val trackCount: Int = 0
+    val isLiked: Boolean = false
 )
