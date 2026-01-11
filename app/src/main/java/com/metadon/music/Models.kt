d914bfaad4b0b7fc6e7c7febf1b05df6d601790f
Metadon-svg <krisevicu@gmail.com>
2026-01-11 13:42:32 +0300

    Update Models.kt

--- app/src/main/java/com/metadon/music/Models.kt
+++ app/src/main/java/com/metadon/music/Models.kt
@@ -5,15 +5,12 @@ data class Track(
     val title: String,
     val artist: String,
     val cover: String,
-    val duration: Int = 0
-)
-
-data class LyricsLine(
-    val time: Double,
-    val text: String
+    val duration: Int = 0,
+    var isLiked: Boolean = false // Добавлено поле для лайка
 )
 
 data class Playlist(
     val id: Int,
-    val name: String
+    val name: String,
+    val trackCount: Int = 0
 )
